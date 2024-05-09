fae780c925977e239d0b8abb5f06db7616f44d25
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-05-09 09:52:35 +0530

    dashboard design completed

--- dashboard_server_action/src/app/dashboard/page.tsx
+++ dashboard_server_action/src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
+import Card from '@/components/ui/dashboard/card/card';
 import React from 'react';
-
+import styles from '@/components/ui/dashboard/dasboard.module.css';
+import RightBar from '@/components/ui/dashboard/rightbar/rightbar';
+import Transactions from '@/components/ui/dashboard/transactions/transactions';
+import Chart from '@/components/ui/dashboard/chart/chart';
 function Dasboard() {
-  return <div>Dasboard</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.card}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
 }
 
 export default Dasboard;
