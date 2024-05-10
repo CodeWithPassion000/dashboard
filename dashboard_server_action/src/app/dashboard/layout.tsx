e76591b737a19f7c8ad0aa5e6f4b1245b96f4629
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-05-10 08:15:57 +0530

    user table produc table and add user add product ui added

--- dashboard_server_action/src/app/dashboard/layout.tsx
+++ dashboard_server_action/src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/ui/dashboard/navbar/navbar';
 import Sidebar from '@/components/ui/dashboard/sidebar/sidebar';
 import React from 'react';
 import styles from '../../components/ui/dashboard/dasboard.module.css';
+import Footer from '@/components/ui/dashboard/footer/footer';
 function Layout({ children }: any) {
   return (
     <div className={styles.container}>
@@ -11,6 +12,7 @@ function Layout({ children }: any) {
       <div className={styles.content}>
         <Navbar />
         {children}
+        <Footer />
       </div>
     </div>
   );
--- dashboard_server_action/src/app/dashboard/products/page.tsx
+++ dashboard_server_action/src/app/dashboard/products/page.tsx
@@ -1,7 +1,108 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/components/ui/dashboard/products/products.module.css';
+import Search from '@/components/ui/dashboard/search/search';
+import Pagination from '@/components/ui/dashboard/pagination/pagination';
+// import { fetchProducts } from "@/app/lib/data";
+// import { deleteProduct } from "@/app/lib/actions";
 
-function Products() {
-  return <div>Products</div>;
-}
+const ProductsPage = async () => {
+  // const q = searchParams?.q || "";
+  // const page = searchParams?.page || 1;
+  // const { count, products } = await fetchProducts(q, page);
 
-export default Products;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Product one
+              </div>
+            </td>
+            <td>This product belongs to men's cloth</td>
+            <td>$80</td>
+            <td>02-04-23</td>
+            <td>10</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+
+          {/* {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || "/noproduct.jpg"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.desc}</td>
+              <td>${product.price}</td>
+              <td>{product.createdAt?.toString().slice(4, 16)}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteProduct}>
+                    <input type="hidden" name="id" value={product.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))} */}
+        </tbody>
+      </table>
+      {/* <Pagination count={count} /> */}
+    </div>
+  );
+};
+
+export default ProductsPage;
